c3838142da6118daa99ee93012d0e45cad8f7d83
skellgreco <tixisb@gmail.com>
2026-03-21 23:42:41 +0200

    feat: Added basic server watching logic

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,11 +1,13 @@
 import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
 import type { Command } from '../types';
+import { checkServers } from '../cron-jobs/serverWatcher';
 
 const command: Command = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
   async execute(interaction: ChatInputCommandInteraction) {
+    await checkServers();
     await interaction.reply('Pong!!');
   },
 };
